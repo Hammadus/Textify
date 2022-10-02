c7c68871e208473c0b40cce6675e9180cbe8af82
Musa Saeed <hsaeed.4209@gmail.com>
2022-10-02 12:55:33 -0400

    Bloomberg text added

---
+++ bloomBerg.js
@@ -0,0 +1,13 @@
+let article = document.querySelector("body > main > div.transporter-item.current > div > article > div > div.content-well__5e77db71 > div.main-column__b9c1cc32 > div.body-content").getElementsByTagName("p")
+let text = [] 
+Array.from(article).forEach(e => { text.push(e.innerText) } );
+
+let link = document.createElement('a');
+link.download = 'article.txt';
+makeTextFile = function (text) {
+    var blob = new Blob([text], {type: 'text/plain'});
+    link.href = URL.createObjectURL(blob);
+    link.click();
+    URL.revokeObjectURL(link.href);
+};
+makeTextFile(text)
