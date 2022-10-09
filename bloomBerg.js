8012016a69b3c36b2ed8b6a4dea3ee738ddfbb51
Hammad Saeed <42039003+Hammadus@users.noreply.github.com>
2022-10-08 20:35:46 -0400

    Update bloomBerg.js

--- bloomBerg.js
+++ bloomBerg.js
@@ -1,4 +1,5 @@
 let article = document.querySelectorAll("body > main > div.transporter-item.current > div > article > div > div.content-well__5e77db71 > div.main-column__b9c1cc32 > div.body-content > p")
+let text = []
 Array.from(article).forEach(e => { text.push(e.innerText) } );
 
 let link = document.createElement('a');
