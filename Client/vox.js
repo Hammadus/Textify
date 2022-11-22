baee94679a3826dd245ba9f7381668a0d7af47d2
Hammad Saeed <hsaeed.4209@gmail.com>
2022-11-21 20:27:28 -0500

    added vox

---
+++ Client/vox.js
@@ -0,0 +1,13 @@
+let article = document.querySelectorAll(".c-entry-content > p");
+let text = []
+Array.from(article).forEach(e => { text.push(e.innerText) } );
+
+let link = document.createElement('a');
+link.download = document.querySelector("title").innerText;
+makeTextFile = function (text) {
+    const blob = new Blob([text.join('\n\n')], {type: 'text/plain', endings: 'native'});
+    link.href = URL.createObjectURL(blob);
+    link.click();
+    URL.revokeObjectURL(link.href);
+};
+makeTextFile(text)
